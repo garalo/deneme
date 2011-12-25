e8761ac7f6c9482857327dc040cb5b2ca91c8b57
Ahmet Kılıç <ahmedkilic@gmail.com>
2011-12-25 12:25:40 +0900

    test commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Deneme::Application.routes.draw do
-  get "home/index"
-
+  #get "home/index"
+  root :to => 'home#index'
   get "home/about"
+  
+  #get "home/new" #asagidaki ile ayni sey
+  match 'home/new' => "home#new"
+  get 'home/create'
+  post '/home/create'
+  get "home/yeni"
+  get "home/ornek"
+  
 
   # get "payments/index"
 
@@ -66,7 +74,7 @@ Deneme::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
